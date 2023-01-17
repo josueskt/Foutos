e3f7a2a03f6f14edb41373132496abc41127d9aa
josueskt <josueskt32@gmail.com>
2023-01-16 22:29:49 -0500

    todas las rutasd

--- src/app.py
+++ src/app.py
@@ -6,6 +6,11 @@ from confg import config
 #rutas 
 from routes import ruta_uno
 from routes import inicio
+from routes import register
+from routes import subir_foto
+from routes import login
+from routes import configuracion
+
 
 app = Flask(__name__)
 def no_encontrado(error):
@@ -17,6 +22,10 @@ app.config.from_object(config['development'])
 #asignacion rutas
 app.register_blueprint(ruta_uno.main , url_prefix='/ruta_uno')
 app.register_blueprint(inicio.main ,url_prefix='/main')
+app.register_blueprint(register.main ,url_prefix='/register/user')
+app.register_blueprint(login.main ,url_prefix='/login/user')
+app.register_blueprint(configuracion.main ,url_prefix='/user/config')
+app.register_blueprint(subir_foto.main ,url_prefix='/upload/foto')
     
 if __name__ == '__main__':
     app.register_error_handler(404 , no_encontrado)
