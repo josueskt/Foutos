eb78124bd485e619f4789f47eac9215170bde3f4
Juan11042001 <104608700+Juan11042001@users.noreply.github.com>
2023-01-18 22:55:17 -0500

    tratando de agregar java

---
+++ src/public/js/almacenar_imagenes.js
@@ -0,0 +1,6 @@
+function colocar_img(){
+var clss = document.getElementsByClassName("form-file");
+var ctx = clss.getContext("2d");
+var img = document.getElementsByClassName("circular--square")
+}
+
