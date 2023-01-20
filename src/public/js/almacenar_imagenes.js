5360d904a113ecc75f55db650888c18e0bac9d99
Juan11042001 <104608700+Juan11042001@users.noreply.github.com>
2023-01-20 01:51:21 -0500

    actualizacion 20-1-23

--- src/public/js/almacenar_imagenes.js
+++
@@ -1,6 +0,0 @@
-function colocar_img(){
-var clss = document.getElementsByClassName("form-file");
-var ctx = clss.getContext("2d");
-var img = document.getElementsByClassName("circular--square")
-}
-
