10e153a04670dfbd35c300b5720596df88775034
josueskt <josueskt32@gmail.com>
2023-01-22 15:54:10 -0500

    todo

--- src/routes/register.py
+++ src/routes/register.py
@@ -1,7 +1,9 @@
-from flask import flash, Blueprint , render_template ,request
-
+from flask import flash, Blueprint , render_template ,request,redirect,url_for
+from database.db import get_Conection
 main=Blueprint('register',__name__)
 
+
 @main.route('/')
-def inicio():
+def register():
+    
      return render_template('register.html')
